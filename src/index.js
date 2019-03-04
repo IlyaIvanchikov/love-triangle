56daea4f3eb53594121a2db0c170b07fc42e6f74
IlyaIvanchikov <phantomonster1@mail.ru>
2019-03-04 22:29:51 +0200

    feaf: add answer

--- src/index.js
+++ src/index.js
@@ -3,5 +3,12 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+    var sum = 0;
+    var i;
+    for ( i = 0; i < preferences.length; i++) {
+    if (preferences[i] == (i+2) && preferences[i+1] == (i+3) && preferences[i+2] == (i+1)) {
+        sum = sum + 1;
+        } 
+    }
+     return sum;
+ };
